4c7e74d5b6bd9eb5cee005986f5a015fa43601b4
Valik <valik3201@gmail.com>
2023-11-04 12:01:11 +0100

    Add: gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryImage = galleryItems.map((element) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery__item");
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = element.preview;
+  image.alt = element.description;
+
+  galleryItem.appendChild(image);
+
+  return galleryItem;
+});
+
+galleryContainer.append(...galleryImage);
